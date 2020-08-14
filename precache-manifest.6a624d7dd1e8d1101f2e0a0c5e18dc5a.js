fab4b84412efe39294904aa9be5f50ba09bcdf0d
klmhotel1313 <anish.garg2296@gmail.com>
2020-08-15 02:19:55 +0530

    Updates

---
+++ precache-manifest.6a624d7dd1e8d1101f2e0a0c5e18dc5a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b165f467d87cc66a516fca90bc3153ac",
+    "url": "/klm-menu/index.html"
+  },
+  {
+    "revision": "75f78f1c53396ddf3d3b",
+    "url": "/klm-menu/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "8ec67f9be3da2664a58e",
+    "url": "/klm-menu/static/css/main.b95c2adf.chunk.css"
+  },
+  {
+    "revision": "75f78f1c53396ddf3d3b",
+    "url": "/klm-menu/static/js/2.5798af7f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/klm-menu/static/js/2.5798af7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ec67f9be3da2664a58e",
+    "url": "/klm-menu/static/js/main.da788ea2.chunk.js"
+  },
+  {
+    "revision": "5bb2553b31bff0a305a7",
+    "url": "/klm-menu/static/js/runtime-main.c8c649db.js"
+  },
+  {
+    "revision": "c486a83ed6689dba93471bbeedc62698",
+    "url": "/klm-menu/static/media/image1.c486a83e.jpg"
+  }
+]);
